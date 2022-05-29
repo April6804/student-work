7aeae81434eb5f524811e7bef33f0883818f3880
April <aprilfultz@gmail.com>
2022-05-29 11:41:07 -0400

    Commented out and brought back code

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,12 +1,12 @@
 <?php
 $errors = '';
-$myemail = 'hello@aprilfultz.com';//<-----Put Your email address here.
-if(empty($_POST['name'])  ||
-   empty($_POST['email']) ||
-   empty($_POST['message']))
-{
-    $errors .= "\n Error: all fields are required";
-}
+$myemail = 'hello@aprilfultz.com';
+// if(empty($_POST['name'])  ||
+//    empty($_POST['email']) ||
+//    empty($_POST['message']))
+// {
+//     $errors .= "\n Error: all fields are required";
+// }
 
 $name = $_POST['name'];
 $email_address = $_POST['email'];
